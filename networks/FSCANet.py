f2a1dc5a7c9830d14d203da2403282e8ca926d8e
Henry991115 <105030589+Henry991115@users.noreply.github.com>
2024-01-10 10:27:39 +0800

    Update FSCANet.py

--- networks/FSCANet.py
+++ networks/FSCANet.py
@@ -64,32 +64,6 @@ class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
 
-class CCA(nn.Module):
-    """
-    CCA Block
-    """
-    def __init__(self, F_g, F_x):
-        super().__init__()
-        self.mlp_x = nn.Sequential(
-            Flatten(),
-            nn.Linear(F_x, F_x))
-        self.mlp_g = nn.Sequential(
-            Flatten(),
-            nn.Linear(F_g, F_x))
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, g, x):
-        # channel-wise attention
-        avg_pool_x = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-        channel_att_x = self.mlp_x(avg_pool_x)
-        avg_pool_g = F.avg_pool2d( g, (g.size(2), g.size(3)), stride=(g.size(2), g.size(3)))
-        channel_att_g = self.mlp_g(avg_pool_g)
-        channel_att_sum = (channel_att_x + channel_att_g)/2.0
-        scale = torch.sigmoid(channel_att_sum).unsqueeze(2).unsqueeze(3).expand_as(x)
-        x_after_channel = x * scale
-        out = self.relu(x_after_channel)
-        return out
-
 
 
 class DPCABlock(nn.Module):
@@ -125,13 +99,11 @@ class UpBlock_attention_high(nn.Module):
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super().__init__()
         self.up = nn.Upsample(scale_factor=2)
-        self.coatt = CCA(F_g=in_channels//2, F_x=in_channels//2)
         self.coatt_1 = DPCABlock(channels=in_channels//2)
         self.nConvs = _make_nConv(in_channels, out_channels, nb_Conv, activation)
 
     def forward(self, x, skip_x):
         up = self.up(x)
-        #skip_x_att = self.coatt(g=up, x=skip_x)
         skip_x_att = self.coatt_1(g=up, x=skip_x)
         x = torch.cat([skip_x_att, up], dim=1)  # dim 1 is the channel dimension
         return self.nConvs(x)
@@ -140,12 +112,10 @@ class UpBlock_attention_low(nn.Module):
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super().__init__()
         self.up = nn.Upsample(scale_factor=2)
-        self.coatt = CCA(F_g=in_channels//2, F_x=in_channels//2)
         self.coatt_1 = DPCABlock(channels=in_channels//2)
         self.nConvs = _make_nConv(in_channels, out_channels, nb_Conv, activation)
 
     def forward(self, x, skip_x):
-        #skip_x_att = self.coatt(g=up, x=skip_x)
         skip_x_att = self.coatt_1(g=x, x=skip_x)
         x = torch.cat([skip_x_att, x], dim=1)  # dim 1 is the channel dimension
         return self.nConvs(x)
@@ -155,7 +125,6 @@ class bridge(nn.Module):
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super().__init__()
         self.up = nn.Upsample(scale_factor=2)
-        #self.coatt = CCA(F_g=in_channels//2, F_x=in_channels//2)
         self.coatt_1 = DPCABlock(channels=in_channels//2)
         self.nConvs = _make_nConv(in_channels, out_channels, nb_Conv, activation)
 
